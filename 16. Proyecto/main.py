c4e2dd4feb672771fe06c1b3827c4c962147f5db
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2024-09-02 12:20:03 -0500

    Refactor(addProduct_Cart) leginilidad del codigo lo que se realizo es por medio de las guard clauses mejorar la legibilidad del codigo para que este ses mucho mas legible

--- 16. Proyecto/main.py
+++ 16. Proyecto/main.py
@@ -67,26 +67,25 @@ def addProduct_Cart(name, cant):
     itemCart = next((cart for cart in carrito if cart["nombre"].lower() == name), None)
     if not product:
       print("Producto no encontrado. Verifica el nombre del producto")
-    if product["stock"] >= cant:
-      confirm = input(f"¿Desea agregar {cant} unidades de {product['nombre']} al carrito? (s/n): ").lower()
-      if confirm == "s":
-        if itemCart:
-          itemCart["cantidad"] += cant
-          itemCart["precio"]  = product["precio"] * itemCart["cantidad"]
-          product["stock"] -= cant
-          print(f"Se ha agregado {cant} unidades de {product['nombre']} al carrito")
-        else: 
-          carrito.append({
-            "nombre": product["nombre"],
-            "cantidad": cant,
-            "precio": product["precio"] * cant
-          })
-          product["stock"] -= cant
-          print(f"Se ha agregado {cant} unidades de {product['nombre']} al carrito")
-      else:
-        print("Operación cancelada")
-    else:
+      return
+    if product["stock"] < cant:
       print("No hay suficiente stock")
+      return
+    confirm = input(f"¿Desea agregar {cant} unidades de {product['nombre']} al carrito? (s/n): ").lower()
+    if confirm != "s":
+      print("Operación cancelada")
+      return
+    if itemCart:
+      itemCart["cantidad"] += cant
+      itemCart["precio"]  = product["precio"] * itemCart["cantidad"]
+    else: 
+      carrito.append({
+        "nombre": product["nombre"],
+        "cantidad": cant,
+        "precio": product["precio"] * cant
+      })
+    product["stock"] -= cant
+    print(f"Se ha agregado {cant} unidades de {product['nombre']} al carrito")
   except Exception as e:
     print("Se ha presentado un error al añadir al carrito", e)
 
